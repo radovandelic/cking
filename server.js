65071b5ee15a26e90f44152ee734b31491128c0c
Radovan Delic <infamouszgbgd@yahoo.com>
2018-03-08 03:47:25 +0100

    static serve

---
+++ server.js
@@ -0,0 +1,30 @@
+/* import http from 'http'
+import express from 'express'
+import path from 'path'
+import compression from 'compression' */
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const compression = require('compression');
+
+const app = express()
+const port = 5000;
+const ip = "0.0.0.0";
+const env = "production";
+
+app.use(compression())
+app.use(express.static('build'))
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
+
+const server = http.createServer(app)
+
+setImmediate(() => {
+    server.listen(port, ip, () => {
+        console.log('Express server listening on http://%s:%d, in %s mode', ip, port, env)
+    })
+})
+
+//export default app
+module.exports = app;
