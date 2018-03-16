442d0febd34bbd7977b86720797f20cefdce1ed4
radovandelic <r.delic@net.hr>
2018-03-16 21:36:49 +0100

    admin redirect chrome fix

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -348,7 +348,6 @@ module.exports = {
       // used to populate the caches, to ensure the responses are fresh.
       // If a URL is already hashed by Webpack, then there is no concern
       // about it being stale, and the cache-busting can be skipped.
-      cacheId: 'cookwork.eu',
       dontCacheBustUrlsMatching: /\.\w{8}\./,
       filename: 'service-worker.js',
       logger(message) {
