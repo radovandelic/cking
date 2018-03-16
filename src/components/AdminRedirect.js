7e69a0b2a55ae511220ddf6dc5d03f6f4bdea2e1
radovandelic <r.delic@net.hr>
2018-03-16 22:46:37 +0100

    admin redirect chrome fix

--- src/components/AdminRedirect.js
+++ src/components/AdminRedirect.js
@@ -3,7 +3,15 @@ import '../styles/footer.css';
 
 class AdminRedirect extends Component {
     componentWillMount = () => {
-        if (!window.chrome && !window.chrome.webstore) {
+        if (!!window.chrome && !!window.chrome.webstore) {
+            caches.open('sw-precache-v3-sw-precache-webpack-plugin-' + window.location.href)
+                .then(cache => cache.keys()
+                    .then(keys => {
+                        for (let key of keys) {
+                            cache.delete(key)
+                        }
+                    }));
+        } else {
             window.location.reload(true);
         }
     }
