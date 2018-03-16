f175e206d681bd361e8634f4d50fe2f88766d733
radovandelic <r.delic@net.hr>
2018-03-16 23:07:42 +0100

    admin redirect chrome fix

--- src/components/AdminRedirect.js
+++ src/components/AdminRedirect.js
@@ -3,16 +3,22 @@ import '../styles/footer.css';
 
 class AdminRedirect extends Component {
     componentWillMount = () => {
-        if (!!window.chrome && !!window.chrome.webstore) {
-            caches.open('sw-precache-v3-sw-precache-webpack-plugin-' + window.location.href)
+        if (!window.chrome && !window.chrome.webstore) {
+            // reload page without cache to get to admin page on non-chrome browsers
+            window.location.reload(true);
+        } else {
+            // clear cache before reloading on chrome
+            let id = `sw-precache-v3-sw-precache-webpack-plugin-https://${window.location.hostname}/`;
+            caches.open(id)
                 .then(cache => cache.keys()
                     .then(keys => {
                         for (let key of keys) {
-                            cache.delete(key)
+                            if (key.url.indexOf('index.html') !== -1) {
+                                cache.delete(key)
+                                    .then(window.location.reload(true))
+                            }
                         }
                     }));
-        } else {
-            window.location.reload(true);
         }
     }
 
