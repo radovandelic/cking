29315f627125585365a2e3a35d1ff8c7bc5e2d0a
radovandelic <r.delic@net.hr>
2018-03-18 13:43:18 +0100

    retrieve inline styles

--- src/containers/Kitchen.js
+++ src/containers/Kitchen.js
@@ -86,18 +86,18 @@ class Kitchen extends Component {
         return (
             <div className="listing-container">
                 <div className="carousel" >
-                    <div id="jssor_1">
+                    <div id="jssor_1" style={{ position: "relative", margin: "0 auto", top: "0px", left: "0px", width: "980px", height: "480px", overflow: "hidden", visibility: "hidden" }}>
                         {/* <!-- Loading Screen --> */}
-                        <div data-u="loading" className="jssorl-009-spin">
-                            <img alt="kitchen" src="/static/img/spin.svg" />
+                        <div data-u="loading" className="jssorl-009-spin" style={{ position: "absolute", top: "0px", left: "0px", width: "100%", height: "100%", textAlign: "center", backgroundColor: "rgba(0,0,0,0.7)" }}>
+                            <img alt="kitchen" style={{ marginTop: "-19px", position: "relative", top: "50%", width: "38px", height: "38px" }} src="/static/img/spin.svg" />
                         </div>
-                        <div data-u="slides">
+                        <div data-u="slides" style={{ cursor: "default", position: "relative", top: "0px", left: "0px", width: "980px", height: "380px", overflow: "hidden" }}>
                             {this.state.gallery}
                         </div>
                         {/* <!-- Thumbnail Navigator --> */}
-                        <div data-u="thumbnavigator" className="jssort101" data-autocenter="1" data-scale-bottom="0.75">
+                        <div data-u="thumbnavigator" className="jssort101" style={{ position: "absolute", left: "0px", bottom: "0px", width: "980px", height: "100px", backgroundColor: "#000" }} data-autocenter="1" data-scale-bottom="0.75">
                             <div data-u="slides">
-                                <div data-u="prototype" className="p" >
+                                <div data-u="prototype" className="p" style={{ width: "190px", height: "90px" }}>
                                     <div data-u="thumbnailtemplate" className="t"></div>
                                     <svg viewBox="0 0 16000 16000" className="cv">
                                         <circle className="a" cx="8000" cy="8000" r="3238.1"></circle>
@@ -108,15 +108,15 @@ class Kitchen extends Component {
                             </div>
                         </div>
                         {/* <!-- Arrow Navigator --> */}
-                        <div data-u="arrowleft" className="jssora106" data-scale="0.75">
-                            <svg viewBox="0 0 16000 16000">
+                        <div data-u="arrowleft" className="jssora106" style={{ width: "55px", height: "55px", top: "162px", left: "30px" }} data-scale="0.75">
+                            <svg viewBox="0 0 16000 16000" style={{ position: "absolute", top: "0", left: "0", width: "100%", height: "100%" }}>
                                 <circle className="c" cx="8000" cy="8000" r="6260.9"></circle>
                                 <polyline className="a" points="7930.4,5495.7 5426.1,8000 7930.4,10504.3 "></polyline>
                                 <line className="a" x1="10573.9" y1="8000" x2="5426.1" y2="8000"></line>
                             </svg>
                         </div>
-                        <div data-u="arrowright" className="jssora106" data-scale="0.75">
-                            <svg viewBox="0 0 16000 16000" >
+                        <div data-u="arrowright" className="jssora106" style={{ width: "55px", height: "55px", top: "162px", right: "30px" }} data-scale="0.75">
+                            <svg viewBox="0 0 16000 16000" style={{ position: "absolute", top: "0", left: "0", width: "100%", height: "100%" }}>
                                 <circle className="c" cx="8000" cy="8000" r="6260.9"></circle>
                                 <polyline className="a" points="8069.6,5495.7 10573.9,8000 8069.6,10504.3 "></polyline>
                                 <line className="a" x1="5426.1" y1="8000" x2="10573.9" y2="8000"></line>
